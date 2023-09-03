809ae557ff0dace225f4a61f2f20509005caed3f
Alishan Shaikh <alishanshaikh9890@gmail.com>
2023-09-03 11:33:35 +0530

    first one

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aaimaa Web Solutions </h1>
+      <h4>get the file</h4>
+      <input type="file" />
+      
+    </div>
+  );
+}
+
+export default App;
